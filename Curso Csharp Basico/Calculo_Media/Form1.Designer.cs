e0a928b93f146a9f5d92e9335d9292e01063a7d3
Luana Karlla Silva <karlla_1911@hotmail.com>
2021-10-04 22:12:40 -0300

    Calcular Média de um aluno

---
+++ Curso Csharp Basico/Calculo_Media/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Calculo_Media
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.textnota1 = new System.Windows.Forms.TextBox();
+            this.textNota2 = new System.Windows.Forms.TextBox();
+            this.textNota3 = new System.Windows.Forms.TextBox();
+            this.textMedia = new System.Windows.Forms.TextBox();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(99, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cálculo de Média do Aluno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(24, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite a 1º Nota:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(24, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Digite a 2º Nota:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(24, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Digite a 3º Nota:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(89, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Média:";
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnCalcular.Location = new System.Drawing.Point(34, 225);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(117, 34);
+            this.BtnCalcular.TabIndex = 5;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // textnota1
+            // 
+            this.textnota1.Location = new System.Drawing.Point(171, 85);
+            this.textnota1.Name = "textnota1";
+            this.textnota1.Size = new System.Drawing.Size(152, 23);
+            this.textnota1.TabIndex = 6;
+            // 
+            // textNota2
+            // 
+            this.textNota2.Location = new System.Drawing.Point(171, 125);
+            this.textNota2.Name = "textNota2";
+            this.textNota2.Size = new System.Drawing.Size(152, 23);
+            this.textNota2.TabIndex = 7;
+            // 
+            // textNota3
+            // 
+            this.textNota3.Location = new System.Drawing.Point(171, 170);
+            this.textNota3.Name = "textNota3";
+            this.textNota3.Size = new System.Drawing.Size(152, 23);
+            this.textNota3.TabIndex = 8;
+            // 
+            // textMedia
+            // 
+            this.textMedia.Location = new System.Drawing.Point(171, 285);
+            this.textMedia.Name = "textMedia";
+            this.textMedia.Size = new System.Drawing.Size(152, 23);
+            this.textMedia.TabIndex = 9;
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnLimpar.Location = new System.Drawing.Point(196, 225);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(117, 34);
+            this.BtnLimpar.TabIndex = 10;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnSair.Location = new System.Drawing.Point(362, 225);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(117, 34);
+            this.BtnSair.TabIndex = 11;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(491, 450);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.textMedia);
+            this.Controls.Add(this.textNota3);
+            this.Controls.Add(this.textNota2);
+            this.Controls.Add(this.textnota1);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.TextBox textnota1;
+        private System.Windows.Forms.TextBox textNota2;
+        private System.Windows.Forms.TextBox textNota3;
+        private System.Windows.Forms.TextBox textMedia;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.Button BtnSair;
+    }
+}
+
---
+++ Curso Csharp Basico/Calculo_Media/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_Media
+{
+    public partial class Form1 : Form
+    {
+        double n1, n2, n3, media;
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            textnota1.Text = "";
+            textNota2.Text = "";
+            textNota3.Text = "";
+            textMedia.Text = "";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(textnota1.Text);
+            n2 = Convert.ToDouble(textNota2.Text);
+            n3 = Convert.ToDouble(textNota3.Text);
+            media = (n1 + n2 + n3) / 3;
+            textMedia.Text = media.ToString();
+
+        }
+    }
+}
