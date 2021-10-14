ef3a9beeffc7f0bec2c320b0409b288151cafeb1
Luana Karlla Silva <karlla_1911@hotmail.com>
2021-10-14 20:23:20 -0300

    Sistema Cantina-em andamento

---
+++ Curso Csharp Basico/Sistema_Cantina/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Sistema_Cantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.listCaixa = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(145, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Cantina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(35, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Código do Produto:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(26, 86);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(177, 23);
+            this.txtCodigo.TabIndex = 2;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(26, 115);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(177, 170);
+            this.picImagem.TabIndex = 3;
+            this.picImagem.TabStop = false;
+            // 
+            // listCaixa
+            // 
+            this.listCaixa.FormattingEnabled = true;
+            this.listCaixa.ItemHeight = 15;
+            this.listCaixa.Location = new System.Drawing.Point(219, 86);
+            this.listCaixa.Name = "listCaixa";
+            this.listCaixa.Size = new System.Drawing.Size(258, 199);
+            this.listCaixa.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(120, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor Total:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 355);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listCaixa);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.ListBox listCaixa;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Curso Csharp Basico/Sistema_Cantina/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Cantina
+{
+    public partial class Form1 : Form
+    {
+        string[] produtos = new string[10];
+        string[] codigo = new string[10];
+        double[] valor = new double[10];
+        double soma;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text.Length==3)
+            {
+                /*listCaixa.Items.Add(txtCodigo.Text);
+                txtCodigo.Text = "";
+                txtCodigo.Focus();*/
+
+                int indice = 0;
+                for(int prod = 1; prod < codigo.Length; prod++)
+                {
+                    if (txtCodigo.Text==codigo[prod])
+                    {
+                        indice = prod;
+                    }
+                }
+                if (indice==0)
+                {
+                    MessageBox.Show("Produto não encontrado");
+                }
+                else
+                {
+                    listCaixa.Items.Add(txtCodigo.Text + "---" + produtos[indice] + "--R$" + valor[indice]);
+                    soma = soma+valor[indice];
+                    label1.Text = ("Valor total R$ " + soma);
+                    picImagem.ImageLocation = "D:/MeusProjetos/Curso CSharp Básico-Fundação Bradesco/Curso-CSharp-Basico/Curso Csharp Basico/Sistema_Cantina/Imagem" + codigo[indice] + ".jpeg";
+                    txtCodigo.Text = "";
+                    txtCodigo.Focus(); 
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarArray();
+            soma = 0;
+        }
+        private void CarregarArray()
+        {
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+            codigo[5] = "005";
+
+            produtos[1] = "Pastel";
+            produtos[2] = "Coxinha";
+            produtos[3] = "Hot Dog";
+            produtos[4] = "Chololate";
+            produtos[5] = "Suco";
+
+            valor[1] = 6.00;
+            valor[2] = 5.00;
+            valor[3] = 12.00;
+            valor[4] = 3.50;
+            valor[5] = 8.00;
+        }
+    }
+}
